114c7fb5e6e9f0a06c518bbc7fa8fa9be4cde145
ldx <you@example.com>
2019-10-31 17:48:34 +0800

    1031tijiao

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 引入路由文件
+import Register from './pages/register'
+import Login from './pages/login'
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<Switch>
+          <Route path="/register" component={Register}></Route>
+          <Route path="/login" component={Login}></Route>
+          
+        </Switch>
+			</div>
+		);
+	}
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+//引入框架文件
+import 'antd-mobile/dist/antd-mobile.css';
+//引入路由
+import { HashRouter } from 'react-router-dom';
+//引入静态资源文件
+import './assets/css/reset.css';
+import './assets/js/rem';
+//配置axios
+import axios from 'axios';
+Component.prototype.$axios = axios;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<HashRouter>
+		<App />
+	</HashRouter>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
